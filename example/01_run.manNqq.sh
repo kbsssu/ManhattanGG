6bb5eaeadfe530f8010977b7d640bfc4a6ff9b00
kbsssu <kbsssu@gmail.com>
2021-01-18 19:39:08 +0900

    ManhattanGG v.0.0.5 alpha - the first commit

---
+++ example/01_run.manNqq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Rscript ../ManhattanGG.R --assoc example.glm.linear --qq --qq-lambda 1.05 --y-lim 10 \
+			--chr "#CHROM" --pos POS --snp ID --pval P \
+			--deco example.deco --deco-color color  --deco-label label --deco-label-color labelcolor \
+			--text-angle 90 \
+			--suggestiveline 1e-05 --point-size 1.2 \
+			--out example
+
